71721764ce4f394667b9598a98ce6aaaf331dfd3
hcaz528 <zsmail528@yahoo.com>
2017-05-27 02:13:15 -0400

    app

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ if (major <= 7 && minor <= 5) {
 }
 
 // import environmental variables from our variables.env file
-// require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' });
 // process.env.NODE_ENV="development"; // NOTE REVIEW TODO TAKE OUT WHEN GOIG TO PRODUCTION
 // console.log("process.env.NODE_ENV: "+ process.env.NODE_ENV);
 // Connect to our Database and handle an bad connections
